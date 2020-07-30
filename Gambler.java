564155fc5587da0966fe235132e521a56e50fbe8
Shubham Chavan <shubhm.chavan007@gmail.com>
2020-07-30 16:12:44 +0530

    Program to restrict Gambler when he cross his winning/lossing amount limit

--- Gambler.java
+++ Gambler.java
@@ -1,38 +1,35 @@
 import java.util.Random;
 
-public class GamblingUC1 {
-	int stakeAmount=100;
-	int betAmount=1,i=1;
-	int maxWinningAmount=200;
+public class Gambler {
+	//Constants
+	private final int STAKE_PER_DAY = 100;
+	private final int BETTING_AMOUNT = 1;
+	private final int MAX_WINNING_AMOUNT = 150;
+	private final int MIN_LOSING_AMOUNT = 50;
 	
+	/*Restriction for player if he reaches Max.Winning amount i.e 150 or he reaches Max. losing amount i.e 50 then stop playing & 	terminates the program*/
 	public void gamblingSimulation() {
-		while (i <= maxWinningAmount)
+		int stakeAmount = STAKE_PER_DAY;
+		
+		while (stakeAmount >= MIN_LOSING_AMOUNT && stakeAmount <= MAX_WINNING_AMOUNT)
 		{
-			if (stakeAmount > 0 && stakeAmount<= maxWinningAmount)
+			Random rand=new Random();
+			int num=rand.nextInt(2);
+			if (num == 1)
 			{
-				Random rand=new Random();
-				int num=rand.nextInt(2);
-				if (num == 1)
-				{
-					stakeAmount=stakeAmount+betAmount;
-				}
-				else
-				{
-					stakeAmount=stakeAmount-betAmount;
-				}
-				
+				stakeAmount=stakeAmount+BETTING_AMOUNT;
 			}
-			i++;
-			System.out.println(stakeAmount);
-			
+			else
+			{
+				stakeAmount=stakeAmount-BETTING_AMOUNT;
+			}
 		}
-
+		System.out.println("You Has Reached to Your Max Win/Loss: Your Amount =: "+stakeAmount);
 	}
-	
-
 
 	public static void main(String[] args) {
-		GamblingUC1 a=new GamblingUC1();
+		//Calling a method via Constructor
+		Gambler a = new Gambler();
 		a.gamblingSimulation();
 
 	}
