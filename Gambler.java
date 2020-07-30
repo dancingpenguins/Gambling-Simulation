a47bd5c62b3c6033993cba1007fb0c7e067dd154
Shubham Chavan <shubhm.chavan007@gmail.com>
2020-07-30 19:45:32 +0530

    Refacoring UC4: for finding Profit & Loss after 20 Days of Playing

--- Gambler.java
+++ Gambler.java
@@ -1,20 +1,22 @@
 import java.util.Random;
 
-public class Gambler {
+public class GamblerUC4 {
 	
 	private final int STAKE_PER_DAY = 100;
 	private final int BETTING_AMOUNT = 1;
 	private final int MAX_WINNING_AMOUNT = 150;
 	private final int MIN_LOSING_AMOUNT = 50;
 	private final int PLAYING_DAYS=20;
+	
 
 	
 	public void gamblingSimulation() {
-		int i;
-		int stakeAmount = STAKE_PER_DAY;
+		int day;
 		int  daily_Profit=0;
-		int daily_Loss=0;
-		for(i = 0;i <= PLAYING_DAYS; i++) {
+		int daily_Loss=0;		
+		
+		for(day = 0;day <= PLAYING_DAYS; day++) {
+			int stakeAmount = STAKE_PER_DAY;
 			while (stakeAmount >= MIN_LOSING_AMOUNT && stakeAmount <= MAX_WINNING_AMOUNT)
 			{
 				Random rand=new Random();
@@ -22,22 +24,28 @@ public class Gambler {
 				if (num == 1)
 				{
 					stakeAmount = stakeAmount + BETTING_AMOUNT;
-					daily_Profit = stakeAmount + daily_Profit;
 				}
 				else
 				{
-					stakeAmount=stakeAmount-BETTING_AMOUNT;
-					daily_Loss = stakeAmount - daily_Loss;
+					stakeAmount = stakeAmount - BETTING_AMOUNT;
 				}
 			}
-			//System.out.println("You Has Reached to Your daily limit for Win/Loss: Your Amount =: "+stakeAmount);
+			System.out.println("You Has Reached to Your daily limit for Win/Loss: Your Amount =: "+stakeAmount);
+			if (stakeAmount > STAKE_PER_DAY)
+			{
+				daily_Profit = daily_Profit + 50 ;
+			}
+			else
+			{
+				daily_Loss = daily_Loss + 50 ;
+			}
 		}
-		System.out.println("At Day "+i+" Profit=: "+daily_Profit);
-		System.out.println("At Day "+i+" Loss=: "+daily_Loss);
+		System.out.println("At Day "+day+" Profit=: "+daily_Profit);
+		System.out.println("At Day "+day+" Loss=: "+daily_Loss);
 	}
 
 	public static void main(String[] args) {
-		Gambler a = new Gambler ();
+		GamblerUC4 a = new GamblerUC4 ();
 		a.gamblingSimulation();
 
 	}
