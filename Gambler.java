f0087039981ecb5fcfc28acca3f815273ea495ce
Shubham Chavan <shubhm.chavan007@gmail.com>
2020-07-30 17:36:36 +0530

    Gambler's program for calculating Winning/lossing amount on each day for 20 days

--- Gambler.java
+++ Gambler.java
@@ -1,35 +1,43 @@
 import java.util.Random;
 
 public class Gambler {
-	//Constants
+	
 	private final int STAKE_PER_DAY = 100;
 	private final int BETTING_AMOUNT = 1;
 	private final int MAX_WINNING_AMOUNT = 150;
 	private final int MIN_LOSING_AMOUNT = 50;
+	private final int PLAYING_DAYS=20;
+
 	
-	/*Restriction for player if he reaches Max.Winning amount i.e 150 or he reaches Max. losing amount i.e 50 then stop playing & 	terminates the program*/
 	public void gamblingSimulation() {
+		int i;
 		int stakeAmount = STAKE_PER_DAY;
-		
-		while (stakeAmount >= MIN_LOSING_AMOUNT && stakeAmount <= MAX_WINNING_AMOUNT)
-		{
-			Random rand=new Random();
-			int num=rand.nextInt(2);
-			if (num == 1)
+		int  daily_Profit=0;
+		int daily_Loss=0;
+		for(i = 0;i <= PLAYING_DAYS; i++) {
+			while (stakeAmount >= MIN_LOSING_AMOUNT && stakeAmount <= MAX_WINNING_AMOUNT)
 			{
-				stakeAmount=stakeAmount+BETTING_AMOUNT;
+				Random rand=new Random();
+				int num=rand.nextInt(2);
+				if (num == 1)
+				{
+					stakeAmount = stakeAmount + BETTING_AMOUNT;
+					daily_Profit = stakeAmount + daily_Profit;
+				}
+				else
+				{
+					stakeAmount=stakeAmount-BETTING_AMOUNT;
+					daily_Loss = stakeAmount - daily_Loss;
+				}
 			}
-			else
-			{
-				stakeAmount=stakeAmount-BETTING_AMOUNT;
-			}
+			//System.out.println("You Has Reached to Your daily limit for Win/Loss: Your Amount =: "+stakeAmount);
 		}
-		System.out.println("You Has Reached to Your Max Win/Loss: Your Amount =: "+stakeAmount);
+		System.out.println("At Day "+i+" Profit=: "+daily_Profit);
+		System.out.println("At Day "+i+" Loss=: "+daily_Loss);
 	}
 
 	public static void main(String[] args) {
-		//Calling a method via Constructor
-		Gambler a = new Gambler();
+		Gambler a = new Gambler ();
 		a.gamblingSimulation();
 
 	}
