8a74116400ce12533396c845d45da234d94537d5
Shubham Chavan <shubhm.chavan007@gmail.com>
2020-07-31 23:14:15 +0530

    Refactoring : for naming conventions & Spaces

--- Gambler.java
+++ Gambler.java
@@ -10,62 +10,62 @@ public class Gambler {
 	private final int BETTING_AMOUNT = 1;
 	private final int MAX_WINNING_AMOUNT = 150;
 	private final int MIN_LOSING_AMOUNT = 50;
-	private final int PLAYING_DAYS=30;
+	private final int PLAYING_DAYS = 30;
 	
-	List <Integer> Gamblers_Monthly_Profit_Loss_Collection = new ArrayList <Integer> ();
-	List <Integer> Gamblers_winning_Luck = new ArrayList <Integer> ();
-	List <Integer> Gamblers_Lossing_Luck = new ArrayList <Integer> ();
+	List <Integer> dailyGambelingAmount = new ArrayList <Integer> ();
+	List <Integer> winningBets = new ArrayList <Integer> ();
+	List <Integer> lossingBets = new ArrayList <Integer> ();
 
 	public void gamblingSimulation() {
 		int day;
-		int  daily_Profit = 0;
-		int daily_Loss = 0;
-		int counterWin = 0,counterLoss = 0;
+		int  dailyProfit = 0;
+		int dailyLoss = 0;
+		int winCounter = 0,lossCounter = 0;
 		
 		for(day = 1;day <= PLAYING_DAYS; day++) {
 			int stakeAmount = STAKE_PER_DAY;
-			while (stakeAmount >= MIN_LOSING_AMOUNT && stakeAmount <= MAX_WINNING_AMOUNT)
+			while (stakeAmount > MIN_LOSING_AMOUNT && stakeAmount < MAX_WINNING_AMOUNT)
 			{
-				Random rand=new Random();
-				int num=rand.nextInt(2);
+				Random rand = new Random();
+				int num = rand.nextInt(2);
 				if (num == 1)
 				{
 					stakeAmount = stakeAmount + BETTING_AMOUNT;
-					counterWin++;
+					winCounter++;
 				}
 				else
 				{
 					stakeAmount = stakeAmount - BETTING_AMOUNT;
-					counterLoss++;
+					lossCounter++;
 				}				
 			}
-			Gamblers_winning_Luck.add(counterWin);
+			winningBets.add(winCounter);
 			//System.out.println("On day: "+day+" Winning Time: "+counterWin);
-			counterWin=0;
-			Gamblers_Lossing_Luck.add(counterLoss);
+			winCounter = 0;
+			lossingBets.add(lossCounter);
 			//System.out.println("On day: "+day+" Lossing Time: "+counterLoss);
-			counterLoss=0;
-			Gamblers_Monthly_Profit_Loss_Collection.add(stakeAmount - STAKE_PER_DAY);
+			lossCounter = 0;
+			dailyGambelingAmount.add(stakeAmount - STAKE_PER_DAY);
 			//System.out.println("You Has Reached to Your daily limit for Win/Loss: Your Amount =: "+stakeAmount);
 			if (stakeAmount > STAKE_PER_DAY)
 			{
-				daily_Profit = daily_Profit + 50 ;
+				dailyProfit = dailyProfit + 50 ;
 				//System.out.println("On day "+day+" Profit: "+daily_Profit);
 			}
 			else
 			{
-				daily_Loss = daily_Loss + 50 ;
+				dailyLoss = dailyLoss + 50 ;
 				//System.out.println("On day "+day+" Loss: "+daily_Loss);
 			}
 		}
-		System.out.println("Monthly Profit & Lost (DayWise) : "+Gamblers_Monthly_Profit_Loss_Collection);
-		System.out.println("Winning Times"+Gamblers_winning_Luck);
-		System.out.println("Lossing Times"+Gamblers_Lossing_Luck);
+		System.out.println("Monthly Profit & Lost (DayWise) : "+dailyGambelingAmount);
+		System.out.println("Winning Times"+winningBets);
+		System.out.println("Lossing Times"+lossingBets);
 	}
 	
 	public void luckiestDay() {
-		int luckyDay = Collections.max(Gamblers_winning_Luck);
-		int position = Gamblers_winning_Luck.indexOf(luckyDay);
+		int luckyDay = Collections.max(winningBets);
+		int position = winningBets.indexOf(luckyDay);
 		/*int localCounter1 = 0;
 		int maxValue = 0;
 		int position = 0;
@@ -82,8 +82,8 @@ public class Gambler {
 	}
 	
 	public void unluckiestDay() {
-		int unluckyDay = Collections.max(Gamblers_Lossing_Luck);
-		int position = Gamblers_Lossing_Luck.indexOf(unluckyDay);
+		int unluckyDay = Collections.max(lossingBets);
+		int position = lossingBets.indexOf(unluckyDay);
 		
 		/*int localCounter2 = 0;
 		int maxValue = 0;
