132d671f2993372df1b4204fee00cb415d943453
Shubham Chavan <shubhm.chavan007@gmail.com>
2020-07-31 13:34:33 +0530

    After winning would player like to continue for next month

--- Gambler.java
+++ Gambler.java
@@ -3,6 +3,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Gambler {
 	
@@ -11,17 +12,14 @@ public class Gambler {
 	private final int MAX_WINNING_AMOUNT = 150;
 	private final int MIN_LOSING_AMOUNT = 50;
 	private final int PLAYING_DAYS=30;
-	
+	int winningPercentage,loosingPercentage;
 	List <Integer> Gamblers_Monthly_Profit_Loss_Collection = new ArrayList <Integer> ();
-	List <Integer> Gamblers_winning_Luck = new ArrayList <Integer> ();
-	List <Integer> Gamblers_Lossing_Luck = new ArrayList <Integer> ();
-
+	
 	public void gamblingSimulation() {
 		int day;
-		int  daily_Profit = 0;
-		int daily_Loss = 0;
-		int counterWin = 0,counterLoss = 0;
-		
+		int  daily_Profit = 0, daily_Loss = 0;
+		int counterWin = 0,counterLoss = 0,profitCounter = 0,lossCounter = 0;
+		//Gambler play's for 30 days with daily limit of profit/loss=50%
 		for(day = 1;day <= PLAYING_DAYS; day++) {
 			int stakeAmount = STAKE_PER_DAY;
 			while (stakeAmount >= MIN_LOSING_AMOUNT && stakeAmount <= MAX_WINNING_AMOUNT)
@@ -39,73 +37,56 @@ public class Gambler {
 					counterLoss++;
 				}				
 			}
-			Gamblers_winning_Luck.add(counterWin);
 			//System.out.println("On day: "+day+" Winning Time: "+counterWin);
 			counterWin=0;
-			Gamblers_Lossing_Luck.add(counterLoss);
 			//System.out.println("On day: "+day+" Lossing Time: "+counterLoss);
 			counterLoss=0;
 			Gamblers_Monthly_Profit_Loss_Collection.add(stakeAmount - STAKE_PER_DAY);
-			//System.out.println("You Has Reached to Your daily limit for Win/Loss: Your Amount =: "+stakeAmount);
+			//Calculating Daily Profit & Loss
 			if (stakeAmount > STAKE_PER_DAY)
 			{
 				daily_Profit = daily_Profit + 50 ;
-				//System.out.println("On day "+day+" Profit: "+daily_Profit);
+				profitCounter++;
+				System.out.println("On day "+day+" Profit: "+daily_Profit);
 			}
 			else
 			{
 				daily_Loss = daily_Loss + 50 ;
-				//System.out.println("On day "+day+" Loss: "+daily_Loss);
+				lossCounter++;
+				System.out.println("On day "+day+" Loss: "+daily_Loss);
 			}
+			winningPercentage = profitCounter * 100 / PLAYING_DAYS;
+			loosingPercentage = lossCounter * 100 / PLAYING_DAYS;
 		}
 		System.out.println("Monthly Profit & Lost (DayWise) : "+Gamblers_Monthly_Profit_Loss_Collection);
-		System.out.println("Winning Times"+Gamblers_winning_Luck);
-		System.out.println("Lossing Times"+Gamblers_Lossing_Luck);
 	}
 	
-	public void luckiestDay() {
-		int luckyDay = Collections.max(Gamblers_winning_Luck);
-		int position = Gamblers_winning_Luck.indexOf(luckyDay);
-		/*int localCounter1 = 0;
-		int maxValue = 0;
-		int position = 0;
-		for (Integer result:Gamblers_winning_Luck)
+	public void nextMonth() {
+		
+		if (winningPercentage > loosingPercentage)
 		{
-			if(maxValue < result)
+			Scanner sc = new Scanner (System.in);
+			System.out.print("Wopuld you like to Continue ? \n If you want to play Press 1 / If you want to exit press 0 \n Enter Your Choice : ");
+			int nextMonth = sc.nextInt();
+			if (nextMonth == 1)
 			{
-				maxValue = result;
-				position = localCounter1;
+				System.out.println("Welcome *** Let's Start Playing ");
 			}
-			localCounter1++;
-		}*/	
-		System.out.println("Luckiest Day for Gambler is Day: "+position);
+			else
+			{
+				System.out.println("Thank You For Playing *** Have a Good Day..! ");
+			}
+		}
+		else
+		{
+			System.out.println("Thank You For Playing *** Have a Good Day..! ");
+		}
 	}
 	
-	public void unluckiestDay() {
-		int unluckyDay = Collections.max(Gamblers_Lossing_Luck);
-		int position = Gamblers_Lossing_Luck.indexOf(unluckyDay);
-		
-		/*int localCounter2 = 0;
-		int maxValue = 0;
-		int position = 0;
-		for (Integer result:Gamblers_Lossing_Luck)
-		{
-			if(maxValue < result)
-			{
-				maxValue = result;
-				position = localCounter2;
-			}
-			localCounter2++;
-		}*/
-		System.out.println("Unluckiest Day for Gambler is Day: "+position);		
-		
-	}
-
 	public static void main(String[] args) {
 		Gambler a = new Gambler ();
 		a.gamblingSimulation();
-		a.luckiestDay();
-		a.unluckiestDay();
+		a.nextMonth();
 
 	}
 
