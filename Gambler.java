722fb277a16b5685d2338d5800830f4d69b0568d
Shubham Chavan <shubhm.chavan007@gmail.com>
2020-07-31 22:49:38 +0530

    Refactoring: for Naming Conventions

--- Gambler.java
+++ Gambler.java
@@ -1,3 +1,4 @@
+package corejava;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -10,16 +11,16 @@ public class Gambler {
 	private final int MIN_LOSING_AMOUNT = 50;
 	private final int PLAYING_DAYS=30;
 	
-	List <Integer> Gamblers_Monthly_Profit_Loss_Collection = new ArrayList <Integer> ();
-
+	List <Integer> dailyGambeling = new ArrayList <Integer> ();
+	//Calculating profit & loss for a month
 	public void gamblingSimulation() {
 		int day;
-		int  daily_Profit=0;
-		int daily_Loss=0;		
+		int dailyProfit=0;
+		int dailyLoss=0;		
 		
 		for(day = 1;day <= PLAYING_DAYS; day++) {
 			int stakeAmount = STAKE_PER_DAY;
-			while (stakeAmount >= MIN_LOSING_AMOUNT && stakeAmount <= MAX_WINNING_AMOUNT)
+			while (stakeAmount > MIN_LOSING_AMOUNT && stakeAmount < MAX_WINNING_AMOUNT)
 			{
 				Random rand=new Random();
 				int num=rand.nextInt(2);
@@ -32,24 +33,21 @@ public class Gambler {
 					stakeAmount = stakeAmount - BETTING_AMOUNT;
 				}
 			}
-			Gamblers_Monthly_Profit_Loss_Collection.add(stakeAmount - STAKE_PER_DAY);
+			dailyGambeling.add(stakeAmount - STAKE_PER_DAY);
 			//System.out.println("You Has Reached to Your daily limit for Win/Loss: Your Amount =: "+stakeAmount);
 			if (stakeAmount > STAKE_PER_DAY)
 			{
-				daily_Profit = daily_Profit + 50 ;
+				dailyProfit = dailyProfit + 50 ;
 			}
 			else
 			{
-				daily_Loss = daily_Loss + 50 ;
+				dailyLoss = dailyLoss + 50 ;
 			}
 		}
-		System.out.println("At Day "+day+" Profit=: "+daily_Profit);
-		System.out.println("At Day "+day+" Loss=: "+daily_Loss);
-		System.out.println("Days Won & Lost: "+Gamblers_Monthly_Profit_Loss_Collection);
-		/*for (Integer result:Gamblers_Monthly_Profit_Loss_Collection)
-		{
-			System.out.println("On day "+day+" he Wins/Losses "+result);
-		}*/
+		System.out.println("At Day "+day+" Profit=: "+dailyProfit);
+		System.out.println("At Day "+day+" Loss=: "+dailyLoss);
+		System.out.println("Days Won & Lost: "+dailyGambeling);
+
 	}
 
 	public static void main(String[] args) {
