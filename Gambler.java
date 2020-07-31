a8c25e2747be81033a83333857babc959e198755
Shubham Chavan <shubhm.chavan007@gmail.com>
2020-07-31 22:07:10 +0530

    Refactoring : for Naming Conventions

--- Gambler.java
+++ Gambler.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
-public class GamblerUC4 {
-	
+public class Gambler {
+	//Constants
 	private final int STAKE_PER_DAY = 100;
 	private final int BETTING_AMOUNT = 1;
 	private final int MAX_WINNING_AMOUNT = 150;
@@ -9,15 +9,15 @@ public class GamblerUC4 {
 	private final int PLAYING_DAYS=20;
 	
 
-	
+	//Calculating proft & loss after 20 Days
 	public void gamblingSimulation() {
 		int day;
-		int  daily_Profit=0;
-		int daily_Loss=0;		
+		int dailyProfit=0;
+		int dailyLoss=0;		
 		
 		for(day = 0;day <= PLAYING_DAYS; day++) {
 			int stakeAmount = STAKE_PER_DAY;
-			while (stakeAmount >= MIN_LOSING_AMOUNT && stakeAmount <= MAX_WINNING_AMOUNT)
+			while (stakeAmount > MIN_LOSING_AMOUNT && stakeAmount < MAX_WINNING_AMOUNT)
 			{
 				Random rand=new Random();
 				int num=rand.nextInt(2);
@@ -33,19 +33,19 @@ public class GamblerUC4 {
 			System.out.println("You Has Reached to Your daily limit for Win/Loss: Your Amount =: "+stakeAmount);
 			if (stakeAmount > STAKE_PER_DAY)
 			{
-				daily_Profit = daily_Profit + 50 ;
+				dailyProfit = dailyProfit + 50 ;
 			}
 			else
 			{
-				daily_Loss = daily_Loss + 50 ;
+				dailyLoss = dailyLoss + 50 ;
 			}
 		}
-		System.out.println("At Day "+day+" Profit=: "+daily_Profit);
-		System.out.println("At Day "+day+" Loss=: "+daily_Loss);
+		System.out.println("At Day "+day+" Profit=: "+dailyProfit);
+		System.out.println("At Day "+day+" Loss=: "+dailyLoss);
 	}
 
 	public static void main(String[] args) {
-		GamblerUC4 a = new GamblerUC4 ();
+		Gambler a = new Gambler ();
 		a.gamblingSimulation();
 
 	}
