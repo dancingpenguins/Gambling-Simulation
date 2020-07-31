4405d1074d2af2fe9d74931c5fccb343de765670
Shubham Chavan <shubhm.chavan007@gmail.com>
2020-07-31 23:21:01 +0530

    Refactoring :for naming Conventions

--- Gambler.java
+++ Gambler.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -11,54 +12,54 @@ public class Gambler {
 	private final int BETTING_AMOUNT = 1;
 	private final int MAX_WINNING_AMOUNT = 150;
 	private final int MIN_LOSING_AMOUNT = 50;
-	private final int PLAYING_DAYS=30;
+	private final int PLAYING_DAYS = 30;
 	int winningPercentage,loosingPercentage;
-	List <Integer> Gamblers_Monthly_Profit_Loss_Collection = new ArrayList <Integer> ();
+	List <Integer> dailyGambelingAmount = new ArrayList <Integer> ();
 	
 	public void gamblingSimulation() {
 		int day;
-		int  daily_Profit = 0, daily_Loss = 0;
-		int counterWin = 0,counterLoss = 0,profitCounter = 0,lossCounter = 0;
+		int  dailyProfit = 0, dailyLoss = 0;
+		int winningCounter = 0,lossingCounter = 0,profitCounter = 0,lossCounter = 0;
 		//Gambler play's for 30 days with daily limit of profit/loss=50%
 		for(day = 1;day <= PLAYING_DAYS; day++) {
 			int stakeAmount = STAKE_PER_DAY;
-			while (stakeAmount >= MIN_LOSING_AMOUNT && stakeAmount <= MAX_WINNING_AMOUNT)
+			while (stakeAmount > MIN_LOSING_AMOUNT && stakeAmount < MAX_WINNING_AMOUNT)
 			{
 				Random rand=new Random();
 				int num=rand.nextInt(2);
 				if (num == 1)
 				{
 					stakeAmount = stakeAmount + BETTING_AMOUNT;
-					counterWin++;
+					winningCounter++;
 				}
 				else
 				{
 					stakeAmount = stakeAmount - BETTING_AMOUNT;
-					counterLoss++;
+					lossingCounter++;
 				}				
 			}
 			//System.out.println("On day: "+day+" Winning Time: "+counterWin);
-			counterWin=0;
+			winningCounter=0;
 			//System.out.println("On day: "+day+" Lossing Time: "+counterLoss);
-			counterLoss=0;
-			Gamblers_Monthly_Profit_Loss_Collection.add(stakeAmount - STAKE_PER_DAY);
+			lossingCounter=0;
+			dailyGambelingAmount.add(stakeAmount - STAKE_PER_DAY);
 			//Calculating Daily Profit & Loss
 			if (stakeAmount > STAKE_PER_DAY)
 			{
-				daily_Profit = daily_Profit + 50 ;
+				dailyProfit = dailyProfit + 50 ;
 				profitCounter++;
-				System.out.println("On day "+day+" Profit: "+daily_Profit);
+				System.out.println("On day "+day+" Profit: "+dailyProfit);
 			}
 			else
 			{
-				daily_Loss = daily_Loss + 50 ;
+				dailyLoss = dailyLoss + 50 ;
 				lossCounter++;
-				System.out.println("On day "+day+" Loss: "+daily_Loss);
+				System.out.println("On day "+day+" Loss: "+dailyLoss);
 			}
 			winningPercentage = profitCounter * 100 / PLAYING_DAYS;
 			loosingPercentage = lossCounter * 100 / PLAYING_DAYS;
 		}
-		System.out.println("Monthly Profit & Lost (DayWise) : "+Gamblers_Monthly_Profit_Loss_Collection);
+		//System.out.println("Monthly Profit & Lost (DayWise) : "+dailyGambelingAmount);
 	}
 	
 	public void nextMonth() {
@@ -71,6 +72,7 @@ public class Gambler {
 			if (nextMonth == 1)
 			{
 				System.out.println("Welcome *** Let's Start Playing ");
+				gamblingSimulation();
 			}
 			else
 			{
